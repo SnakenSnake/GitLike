d60cb8764d3ba1d46763b111adaede69f42adf57
SnakenSnake <atharva8sinha6@gmail.com>
2026-06-23 16:50:20 +0000

    Convert submodule to regular folder

---
+++ codecrafters-shell-cpp/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // TODO: Uncomment the code below to pass the first stage
+  do
+  {
+  std::cout << "$ ";
+  std::string user_input;
+  std::cin>>user_input;
+  if(user_input=="exit")
+  {
+    break;
+  }
+  std::cout<<user_input<<": command not found"<<"\n";
+  }
+  while(true);
+}
