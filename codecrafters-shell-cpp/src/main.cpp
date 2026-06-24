013bbfce3014283db870e4122c19d206c02b4617
Atharva <atharva8sinha6@gmail.com>
2026-06-24 22:00:32 +0530

    Add type functionality to find executable files in given directories

--- codecrafters-shell-cpp/src/main.cpp
+++ codecrafters-shell-cpp/src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <cstdlib>
+#ifdef _WIN32 // Identifies if os is Windows
+    #include <io.h>
+#else // If not Windows then os is Linux
+    #include <unistd.h>
+#endif
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -26,10 +33,40 @@ int main() {
   }
   else if(user_input.substr(0,5)=="type ")
   {
-    if(std::find(builtin.begin(),builtin.end(),user_input.substr(5))!=builtin.end())
+    bool type_found=std::find(builtin.begin(),builtin.end(),user_input.substr(5))!=builtin.end();
+    if(type_found)
         std::cout<<user_input.substr(5)<<" "<<"is a shell builtin"<<'\n';
-    else
-        std::cout<<user_input.substr(5)<<": not found"<<"\n";
+    else 
+    {
+      std::string path=getenv("PATH");
+      std::istringstream ss(path);
+      std::string dir;
+      #ifdef _WIN32
+      while(getline(ss,dir,';'))
+      {
+        std::string full_path=dir+"/"+user_input.substr(5);
+        if(!_access(full_path.c_str(),0))
+        {
+          std::cout<<user_input.substr(5)<<" "<<"is "<<full_path<<'\n';
+          type_found=true;
+          break;
+        }
+      }
+      #else
+      while(getline(ss,dir,':'))
+      {
+        std::string full_path=dir+"/"+user_input.substr(5);
+        if(!access(full_path.c_str(),X_OK))
+        {
+          std::cout<<user_input.substr(5)<<" "<<"is "<<full_path<<'\n';
+          type_found=true;
+          break;
+        }
+      }
+      #endif
+    }
+    if(!type_found)
+      std::cout<<user_input.substr(5)<<": not found"<<"\n";
   }
   else
   std::cout<<user_input<<": command not found"<<"\n";
